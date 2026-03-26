c670fb90cc02f7d2bdc23d0aabb06e6a4f227b9d
Raposooalgo <alexandreraposocasas@gmail.com>
2026-03-26 12:41:02 +0100

    Primer commit

---
+++ shared/src/commonMain/kotlin/com/example/higia/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.higia
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
---
+++ shared/src/iosMain/kotlin/com/example/higia/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.higia
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
